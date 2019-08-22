ef11c3475369733f42135033519dc8cf8a27007b
Officialtanc <officialtanc@hotmail.com>
2019-08-22 09:39:11 +0100

    Added - Responsive Tranparent Navbar changes to solid white on scroll

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Change navbar colour on scroll + set fixed top
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height())
+  });
+});
